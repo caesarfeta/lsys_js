78d452fe21f556081bb022f64bc65ed7f7a6eb0d
caesarfeta <adamtavares@gmail.com>
2013-11-15 09:57:48 -0500

    add padding to autocanvas.

--- src/js/LSYS.js
+++ src/js/LSYS.js
@@ -7,7 +7,8 @@ var LSYS = LSYS || { REVISION: '1' }
 /**
  * Build a full screen canvas
 */
-AutoCanvas = function( _id ) {
+AutoCanvas = function( _id, _padding ) {
+	this.padding = ( _padding != undefined ) ? _padding : 0;
 	this.canvas = document.createElement("canvas");
 	document.body.appendChild( this.canvas );
 	this.canvas.id = _id;
@@ -17,8 +18,11 @@ AutoCanvas = function( _id ) {
 	  	self.resize();
 	});}
 AutoCanvas.prototype.resize = function() {
-	this.canvas.width = document.body.clientWidth;
-	this.canvas.height = document.body.clientHeight;
+	this.canvas.width = document.body.clientWidth - this.padding;
+	this.canvas.height = document.body.clientHeight - this.padding;
+	this.canvas.style.position = "fixed";
+	this.canvas.style.top = this.padding/2;
+	this.canvas.style.left = this.padding/2;
 }
 
 /**
