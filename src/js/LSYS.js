613f958f88bc7755b09a34731324d36960e50fd4
caesarfeta <adamtavares@gmail.com>
2013-12-20 17:41:52 -0500

    Removed AutoCanvas that code now lives in jslib.

--- src/js/LSYS.js
+++ src/js/LSYS.js
@@ -3,28 +3,6 @@
 */
 var LSYS = LSYS || { REVISION: '1' }
 
-
-/**
- * Build a full screen canvas
-*/
-AutoCanvas = function( _id, _padding ) {
-	this.padding = ( _padding != undefined ) ? _padding : 0;
-	this.canvas = document.createElement("canvas");
-	document.body.appendChild( this.canvas );
-	this.canvas.id = _id;
-	this.resize();
-	var self = this;
-	window.addEventListener('resize', function( _event ) {
-	  	self.resize();
-	});}
-AutoCanvas.prototype.resize = function() {
-	this.canvas.width = document.body.clientWidth - this.padding;
-	this.canvas.height = document.body.clientHeight - this.padding;
-	this.canvas.style.position = "fixed";
-	this.canvas.style.top = this.padding/2;
-	this.canvas.style.left = this.padding/2;
-}
-
 /**
  * The L-System generator
 */	
